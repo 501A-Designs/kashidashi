4c4882d8b956dd51064561b4649d18c7006c5477
501A <art71903design@gmail.com>
2022-07-15 19:55:05 +0900

    added calendar range select

--- pages/reserve/[id].js
+++ pages/reserve/[id].js
@@ -4,8 +4,7 @@ import React,{useState} from 'react'
 import Header from '../../lib/Header'
 import DispenseKashidashiObject from '../../lib/DispenseKashidashiObject'
 
-
-import { FiEdit,FiHome, FiShield } from "react-icons/fi";
+import { FiBookmark, FiEdit,FiHome, FiShield, FiXCircle } from "react-icons/fi";
 
 import {app} from '../../firebase'
 import { getFirestore, doc, setDoc, updateDoc, collection  } from "firebase/firestore";
@@ -24,6 +23,17 @@ import Nothing from '../../lib/scene/Nothing';
 import { useMediaQuery } from 'react-responsive'
 import Head from 'next/head';
 
+import Modal from 'react-modal';
+import { modalStyle } from '../../lib/style/modalStyle';
+import AlignItems from '../../lib/style/AlignItems';
+import Input from '../../lib/Input';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange } from 'react-date-range';
+import {ja} from 'react-date-range/dist/locale';
+import TextArea from '../../lib/TextArea';
+
 
 export default function ReservationRoom() {
     const router = useRouter();
@@ -34,34 +44,62 @@ export default function ReservationRoom() {
     const [user] = useAuthState(auth);
     const db = getFirestore(app);
 
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [reserveModalSection, setReserveModalSection] = useState('');
+    const [reserveModalObject, setReserveModalObject] = useState('');
+    const [reservationReasonInput, setReservationReasonInput] = useState('');
+
     const roomDataDocumentRef = doc(db, `rooms/${reservationRoomId && reservationRoomId}/`)
     const [roomData] =  useDocument(roomDataDocumentRef);
 
     const reviewsCollectionRef = collection(db, `rooms/${reservationRoomId && reservationRoomId}/reservationObjects/`);
     const [reservationObjects] = useCollection(reviewsCollectionRef);
     
-    const reserveKashidashiObject = async(docObject) =>{
-        let timeNow = moment().format('MMMM Do YYYY, h:mm a');
-        await updateDoc(doc(db, `rooms/${reservationRoomId && reservationRoomId}/reservationObjects/${docObject.id}/`), {
-            reserved:true,
-            reservedBy:user && user.displayName,
-            reservedByUid:user && user.uid,
-            reservedByEmail:user && user.email,
-            reservedByPhoto: user && user.photoURL,
-            reservedTime:timeNow,
-        });
-        await setDoc(doc(db, `user/${user && user.uid}/reservedObjects/${docObject.id}/`), {
-            emoji:docObject.data().emoji,
-            title:docObject.data().title,
-            place:docObject.data().place,
-            due:docObject.data().due,
-            reservedTime:timeNow,
-            reservedRoomId:reservationRoomId
-        });
+    const reserveKashidashiObject = async() =>{
+        const check = confirm(`本当に借りますか？`);
+        if (check) {            
+            let timeNow = moment().format('MMMM Do YYYY, h:mm a');
+            await updateDoc(doc(db, `rooms/${reservationRoomId && reservationRoomId}/reservationObjects/${reserveModalObject.id}/`), {
+                reserved:true,
+                reservedBy:user && user.displayName,
+                reservedByUid:user && user.uid,
+                reservedByEmail:user && user.email,
+                reservedByPhoto: user && user.photoURL,
+                reservedTime:timeNow,
+                reservedSingleDate:calendarState[0].startDate === calendarState[0].endDate ? true:false,
+                reservedSlotStart:calendarState[0].startDate,
+                reservedSlotEnd:calendarState[0].endDate,
+                reservedReason:reservationReasonInput,
+            });
+            await setDoc(doc(db, `user/${user && user.uid}/reservedObjects/${reserveModalObject.id}/`), {
+                emoji:reserveModalObject.data().emoji,
+                title:reserveModalObject.data().title,
+                place:reserveModalObject.data().place,
+                due:reserveModalObject.data().due,
+                reservedTime:timeNow,
+                reservedRoomId:reservationRoomId,
+                reservedSingleDate:calendarState[0].startDate === calendarState[0].endDate ? true:false,
+                reservedSlotStart:calendarState[0].startDate,
+                reservedSlotEnd:calendarState[0].endDate,
+                reservedReason:reservationReasonInput,
+            });
+        }else{
+            alert("予約をキャンセルしました。")
+        }
     }
 
     const isSmallScreen = useMediaQuery({ query: '(max-width: 1200px)' });
     const isVerySmallScreen = useMediaQuery({ query: '(max-width: 800px)' });
+
+    
+    const [calendarState, setCalendarState] = useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+    ]);
+    console.log(calendarState);
 
     return (
         <>
@@ -75,8 +113,72 @@ export default function ReservationRoom() {
                 <title>貸し出し中</title>
                 <meta property="og:title" content="ディスペンスモードの貸し出し画面" key="title" />
             </Head>
+
             {user ? 
                 <>
+                    <Modal
+                        isOpen={modalIsOpen}
+                        style={modalStyle}
+                    >
+                        <Button
+                            onClick={()=>setModalIsOpen(false)}
+                            icon={<FiXCircle/>}
+                            float={'right'}
+                        />
+                        <h2>「{reserveModalObject && reserveModalObject.data().title}」を予約</h2>
+                        <form
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: '1fr',
+                                gap: '0.5em'
+                            }}
+                        >
+                        {reserveModalSection === 'date' &&
+                            <>
+                                <div
+                                    style={{
+                                        border: '2px solid #f0f0f0',
+                                        padding: '1em',
+                                        borderRadius: '10px',
+                                    }}
+                                    >
+                                    <DateRange
+                                        rangeColors={['var(--accentColor)','var(--faintAccentColor','#f0f0f0']}
+                                        editableDateInputs={true}
+                                        onChange={item => setCalendarState([item.selection])}
+                                        moveRangeOnFirstSelection={false}
+                                        ranges={calendarState}
+                                        locale={ja}
+                                    />
+                                </div>
+                                <Button
+                                    accentColor={true}
+                                    // icon={<FiRefreshCw/>}
+                                    onClick={() => setReserveModalSection('reserve')}
+                                >
+                                    次へ
+                                </Button>
+                            </>
+                        }
+                        {reserveModalSection === 'reserve' &&
+                            <>
+                                <p>予約する上での使用目的を記入する必要がございます。できるだけ具体的に書いてください。</p>
+                                <TextArea
+                                    placeholder="〇〇をするため"
+                                    value={reservationReasonInput}
+                                    onChange={(e) => setReservationReasonInput(e.target.value)}
+                                />
+                                <Button
+                                    accentColor={true}
+                                    icon={<FiBookmark/>}
+                                    onClick={() => reserveKashidashiObject()}
+                                >
+                                    予約
+                                </Button>
+                            </>
+                        }
+                        </form>
+                    </Modal> 
                     {roomData && reservationObjects && roomData.data().emailGroup.split('@')[1] === user.email.split('@')[1] ? 
                         <>
                             <Header
@@ -110,8 +212,10 @@ export default function ReservationRoom() {
                                             key={doc.id}
                                             doc={doc}
                                             reserveOnClick={()=>{
-                                                alert(`${doc.data().title}を借りる`);
-                                                reserveKashidashiObject(doc);
+                                                setReserveModalObject(doc);
+                                                setModalIsOpen(true);
+                                                setReserveModalSection('date');
+                                                // reserveKashidashiObject(doc);
                                             }}
                                             reservedBy={doc.data().reservedBy}
                                             reservedByUid = {doc.data().reservedByUid}
